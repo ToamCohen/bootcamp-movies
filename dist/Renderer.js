383747ca5d73039e297976010540aa5d8d9e515a
toam <toamcohen.transformation@gmail.com>
2020-11-24 13:16:56 +0200

    fix

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -5,12 +5,4 @@ class Renderer {
 		const someHTML = template(data);
 		$(`#${containerName}`).empty().append(someHTML);
 	}
-
-
-	renderRating(data, templateName, containerName){
-		const source = $(`#${templateName}-template`).html();
-		const template = Handlebars.compile(source);
-		const someHTML = template(data);
-		$(`${containerName}`).append(someHTML)
-	}
 }
